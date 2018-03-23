8e6c4b28db48f4fbc0592284c924650b4267267b
Daniel Rosowski <daniel.rosowski.extern@postadress.de>
2018-03-23 17:30:12 +0100

    Added logic to calculate score

--- src/cljbowl/core.clj
+++ src/cljbowl/core.clj
@@ -13,5 +13,19 @@
     (conj frames [pins])
     (conj (butlast frames) (conj (last frames) pins))))
 
+(defn calc-frame [current nframes]
+  (cond
+    (empty? nframes) (apply + current)
+    ; strike
+    (= 1 (count current)) (+ 10 (calc-frame (first nframes) (rest nframes)))
+    ; spare
+    (= 10 (apply + current)) (+ 10 ((first nframes) 0))
+    :else (apply + current)))
 
-
+(defn calc-score [frames]
+  (loop [[f & r] frames
+         score []]
+    (if (nil? f)
+      score
+      (recur r (conj score (calc-frame f r))))))
+      
--- test/cljbowl/core_test.clj
+++ test/cljbowl/core_test.clj
@@ -17,4 +17,29 @@
 
 (deftest rolling
   (testing "empty game"
-    (is (= [1] (roll [[]] 1)))))
+    (is (= [[1]] (roll [[]] 1))))
+  (testing "partial frame"
+    (is (= [[5 5]] (roll [[5]] 5))))
+  (testing "new frame"
+    (is (= [[5 5] [5]] (roll [[5 5]] 5)))))
+
+
+(deftest calcframe
+  (testing "empty next frame"
+    (is (= 8 (calc-frame [4 4] []))))
+  (testing "simple frame"
+    (is (= 8 (calc-frame [4 4] [[5 1]]))))
+  (testing "first frame spare"
+    (is (= 15 (calc-frame [4 6] [[5 1]]))))
+  (testing "first frame strike"
+    (is (= 16 (calc-frame [10] [[5 1]]))))
+  (testing "three strikes in a row"
+    (is (= 36 (calc-frame [10] [[10] [10] [5 1]])))))
+
+(deftest calcscore
+  (testing "simple score"
+    (is (= [5 8 10] (calc-score [[1 4] [4 4] [0 10]]))))
+  (testing "spare"
+    (is (= [14 5] (calc-score [[5 5] [4 1]]))))
+  (testing "strike strike spare"
+    (is (= [34 24 14 5] (calc-score [[10] [10] [5 5] [4 1]])))))
